268dd7563d23592f2c9052cd8acdb4c503ce4d2e
starb520 <97459015+starb520@users.noreply.github.com>
2025-11-05 14:50:39 -0700

    completed a portion of the assignment

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+import { Document } from '../document.model';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'cms-document-edit',
   standalone: false,
   templateUrl: './document-edit.component.html',
   styleUrl: './document-edit.component.css'
 })
-export class DocumentEditComponent {
+export class DocumentEditComponent implements OnInit {
+  originalDocument: Document;
+  document: Document;
+  editMode: boolean = false;
+
+  constructor() {}
+
+  ngOnInit(){}
+
+  onSubmit(form: NgForm) {}
+  
+  onCancel() {}
+
 
 }
